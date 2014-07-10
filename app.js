db7fb79004f5909813e5be730b2bfc108cf2c923
Harry Maclean <harryjmaclean@googlemail.com>
2014-07-10 22:01:35 +0100

    first working prototype

--- app.js
+++ app.js
@@ -1,52 +1,74 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var crypto = require('crypto');
+var redis = require('redis');
+var client = redis.createClient();
 
-var words;
 // Set up wordlist
+var words;
 fs.readFile('words.txt', function(err, data) {
   words = data.toString().split(/\n/);
 });
 
-var getURL = function(key) {
-  // Get url from datastore
-  return "https://www.github.com";
-}
-
 var handle = function(req, res) {
-  var path = url.parse(req.url).path;
+  var url_obj = url.parse(req.url);
+  var path = url_obj.pathname;
   if (path === "/new") {
-    // Get url from req params
-    // Store url=> phrase in datastore
-    res.write(createPhrase());
-    res.end();
+    handleNew(req, res);
+    return;
   }
-  var redirect = getURL(path.slice(1, path.length));
-  res.writeHead(302, {"Location": redirect});
-  res.end();
-}
+  handleRedirect(req, res);
+};
+
+var handleRedirect = function(req, res) {
+  var path = url.parse(req.url).pathname;
+  var phrase = path.slice(1, path.length);
+  client.get(phrase, function(err, redirect) {
+    res.writeHead(302, {"Location": redirect});
+    res.end();
+  });
+};
+
+var handleNew = function(req, res) {
+  var qry = url.parse(req.url).query;
+  if (qry === undefined) {
+    res.end();
+    return;
+  }
+  var orig_url = qry.match(/url=(.+)/);
+  if (orig_url === null) {
+    res.end();
+    return;
+  }
+  orig_url = orig_url[1];
+  (function storeURL() {
+    var phrase = createPhrase();
+    client.exists(phrase, function(err,exists) {
+      if (exists === 0) {
+        client.set(phrase, orig_url);
+        res.end(phrase);
+      }
+      else {
+        storeURL();
+      }
+    });
+  })();
+};
 
 var createPhrase = function() {
   // Pick a random combination of 4 words
   var picks = [];
   for (var i = 0; i < 4; i++) {
     picks.push(pickRandomWord());
-  };
+  }
   var phrase = picks.join("-");
-  // Hash it
-  var shasum = crypto.createHash('sha1');
-  shasum.update(phrase);
-  var hash = shasum.digest('hex');
-  // Check that hash is unique
-  // ???
   return phrase;
-}
+};
 
 var pickRandomWord = function() {
   var i = Math.round(Math.random()*words.length);
   return words[i];
-}
+};
 
 var server = http.createServer(handle);
 server.listen(8888);
